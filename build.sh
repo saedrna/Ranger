6e7bd194da58f69e2e3d2932261a4ecdcc1574c3
han <huhan8807@gmail.com>
2020-02-10 20:28:23 +0800

    加入 data

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd build
 cmake -GNinja \
       -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
       -DCMAKE_SYSROOT=$CONDA_PREFIX/$HOST/sysroot \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DCMAKE_BUILD_TYPE=Debug \
       -DMosek_INCLUDE_DIR=/home/han/mosek/9.1/tools/platform/linux64x86/h \
       -DMosek_LIBRARY_RELEASE=/home/han/mosek/9.1/tools/platform/linux64x86/bin/libmosek64.so \
       -DMosek_LIBRARY_DEBUG=/home/han/mosek/9.1/tools/platform/linux64x86/bin/libmosek64.so \
